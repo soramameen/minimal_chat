21c131165743648553e368686c90573f910159ee
中嶋空偉 <sorai.s.nakajima@gmail.com>
2025-12-26 21:11:41 +0900

    init

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,77 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: %i[ show edit update destroy ]
+
+  # GET /messages or /messages.json
+  # app/controllers/messages_controller.rb
+
+  def index
+    @messages = Message.all
+    # ↓ この1行を追加してください。
+    @message = Message.new 
+  end
+  # GET /messages/1 or /messages/1.json
+  def show
+  end
+
+  # GET /messages/new
+  def new
+    @message = Message.new
+  end
+
+  # GET /messages/1/edit
+  def edit
+  end
+
+  # POST /messages or /messages.json
+  def create
+    @message = Message.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        # リダイレクトではなく、フォームをリセットするレスポンスを返す
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            Message.new, 
+            partial: "messages/form", 
+            locals: { message: Message.new }
+          )
+        end
+        format.html { redirect_to messages_url }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end  # PATCH/PUT /messages/1 or /messages/1.json
+  def update
+    respond_to do |format|
+      if @message.update(message_params)
+        format.html { redirect_to @message, notice: "Message was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @message }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /messages/1 or /messages/1.json
+  def destroy
+    @message.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to messages_path, notice: "Message was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_message
+      @message = Message.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def message_params
+      params.expect(message: [ :content ])
+    end
+end
